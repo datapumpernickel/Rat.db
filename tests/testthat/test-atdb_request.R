96213018e9c59035444a5e91c6df93ac28bbc740
datapumpernickel <65470117+datapumpernickel@users.noreply.github.com>
2025-02-10 10:32:54 +0100

    remove stringr dependency

--- tests/testthat/test-atdb_request.R
+++ tests/testthat/test-atdb_request.R
@@ -2,12 +2,9 @@ testthat::test_that('test that build returns a httr2 request',{
   req <- Rat.db:::atdb_build_request(start_year = 2000, end_year = 2001)
   testthat::expect_equal(class(req),'httr2_request')
 
-  testthat::expect_true(stringr::str_detect(req$url,
-                                            'https://atbackend.sipri.org/api/p/trades/trade-register-csv/'))
-  testthat::expect_true(stringr::str_detect(req$body$data,
-                                            '"value2":2001'))
-  testthat::expect_true(stringr::str_detect(req$body$data,
-                                            '"value1":2000'))
+  testthat::expect_true(grepl('https://atbackend.sipri.org/api/p/trades/trade-register-csv/', req$url))
+  testthat::expect_true(grepl('"value2":2001', req$body$data))
+  testthat::expect_true(grepl('"value1":2000', req$body$data))
 })
 
 httptest2::with_mock_dir("test_data",simplify = F, {
